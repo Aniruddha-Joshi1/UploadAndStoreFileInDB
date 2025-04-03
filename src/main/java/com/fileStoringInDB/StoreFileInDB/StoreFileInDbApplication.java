8e05601cd4646437083275e745be2db583025a09
aniruddha.joshi@kanerika.com-at-972218571398 <aniruddha.joshi@kanerika.com>
2025-04-03 16:32:24 +0530

    initialise repo

---
+++ src/main/java/com/fileStoringInDB/StoreFileInDB/StoreFileInDbApplication.java
@@ -0,0 +1,13 @@
+package com.fileStoringInDB.StoreFileInDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreFileInDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreFileInDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fileStoringInDB/StoreFileInDB/StoreFileInDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fileStoringInDB.StoreFileInDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreFileInDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
